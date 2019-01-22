14faba72d8090a27c43c1b9cb1203cbdd08204d7
Lisa Webb <lwebbnyc@outlook.com>
2019-01-22 23:22:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,14 @@
+def position_taken?(board,index)
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+    false
+   else
+    true
+  end
+end
+
+
 def valid_move?(board,index)
- if index.between?(0,8)
+ if (index.between?(0,8)) && (position_taken? == false)
    true
  else
   false
